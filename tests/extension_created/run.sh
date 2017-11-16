8e4aa5f6f42a7a92fbbc984f16943276adfdf095
Leo Antunes <leo@costela.net>
2017-11-16 16:35:38 +0100

    reuse code from official-images for tests

--- tests/extension_created/run.sh
+++ tests/extension_created/run.sh
@@ -1,3 +1,28 @@
 #!/bin/bash
+set -eo pipefail
 
-docker run --rm -u postgres $1 psql -A -c '\x on' -c '\dx' | grep ^Name
+image="$1"
+
+export POSTGRES_USER='my cool postgres user'
+export POSTGRES_PASSWORD='my cool postgres password'
+export POSTGRES_DB='my cool postgres database'
+
+cname="postgres-container-$RANDOM-$RANDOM"
+cid="$(docker run -d -e POSTGRES_USER -e POSTGRES_PASSWORD -e POSTGRES_DB --name "$cname" "$image")"
+trap "docker rm -vf $cid > /dev/null" EXIT
+
+psql() {
+    docker run --rm -i \
+        --link "$cname":postgres \
+        --entrypoint psql \
+        -e PGPASSWORD="$POSTGRES_PASSWORD" \
+        "$image" \
+        --host postgres \
+        --username "$POSTGRES_USER" \
+        --dbname "$POSTGRES_DB" \
+        --quiet --no-align --tuples-only \
+        "$@"
+}
+
+. "~/official-images/retry.sh" --tries "$POSTGRES_TEST_TRIES" --sleep "$POSTGRES_TEST_SLEEP" "psql -c '\x on' -c '\dx' | grep ^Name"
+
