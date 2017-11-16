59d3f8df2b88dd58e50f587915cef05f0216cbc6
Leo Antunes <leo@costela.net>
2017-11-16 16:19:59 +0100

    fix extension creation

--- create_extension_postgis.sh
+++ create_extension_postgis.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-psql -v ON_ERROR_STOP=1 --username $POSTGRES_USER $POSTGRES_DB <<- EOF
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" "$POSTGRES_DB" <<- EOF
     CREATE EXTENSION IF NOT EXISTS postgis;
     CREATE EXTENSION IF NOT EXISTS postgis_topology;
     CREATE EXTENSION IF NOT EXISTS address_standardizer;
